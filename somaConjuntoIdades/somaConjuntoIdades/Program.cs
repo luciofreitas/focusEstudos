49e1c3598bbb400bd5e4f3841b0541b0e72892af
luciofreitas <luciodfp@gmail.com>
2023-05-15 13:37:21 -0300

    somaConjuntoIdades

---
+++ somaConjuntoIdades/somaConjuntoIdades/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somaConjuntoIdades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a quantidade de idades voce quer que some: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] idades = new int[n];
+
+            for (int i = 0; i < idades.Length; i++)
+            {
+                Console.Write($"Informe a {i + 1}ª idade: ");
+                idades[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            for (int i = 0; i < idades.Length; i++)
+            {
+                sum += idades[i];
+            }
+            Console.WriteLine($"A soma das {n} idade é igual a: {sum}");
+
+            Console.ReadLine();
+        }
+    }
+}
