dee65559279c64e9b21a19f3999edf88e49024d3
luciofreitas <luciodfp@gmail.com>
2023-05-15 10:58:33 -0300

    salarioPorHora

---
+++ salarioPorHora/salarioPorHora/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salarioPorHora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe seu nome, por favor: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Informe seu salario mensal: ");
+            double salarioMensal = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe sua carga horária: ");
+            int hora = int.Parse(Console.ReadLine());
+
+            double salarioPorHora = salarioMensal / (double)hora;
+
+            Console.WriteLine($"O salario por hora do {nome} é igual a R${salarioPorHora}");
+
+            Console.ReadLine();
+        }
+    }
+}
